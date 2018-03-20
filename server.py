4d708e928695445f420994fb0430e111c04ba5ad
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-20 13:02:59 -0700

    completed /hello and /diss

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 """Greeting Flask app."""
 
-from random import choice
-
 from flask import Flask, request
 
 # "__name__" is a special Python variable for the name of the current module
@@ -13,17 +11,39 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+DISS = ['stinky', 'lame-sauce', 'derp']
+
 
 @app.route('/')
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+          <!doctype html>
+          <html>
+            <head>
+            </head>
+            <body>
+              Hi! This is the home page.<br>
+              <a href="/hello">Hello Form</a>
+            </body>
+          </html>
+          """
 
 
 @app.route('/hello')
 def say_hello():
     """Say hello and prompt for user's name."""
+
+    comp_options = ""
+    for compliment in AWESOMENESS:
+        option = "<option value='{}'>{}</option>".format(compliment, compliment)
+        comp_options = comp_options + option
+
+    diss_options = ""
+    for diss in DISS:
+        option = "<option value='{}'>{}</option>".format(diss, diss)
+        diss_options = diss_options + option
 
     return """
     <!doctype html>
@@ -34,12 +54,30 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
+          What's your name? <input type="text" name="person"><br>
+          <br>
+          Pick a Compliment:
+          <select name="compliment">
+            {compliment}
+          </select><br>
+          <br>
+          <input type="submit" value="Get Compliment">
+
+        </form><br>
+        <form action="/diss">
+          What's your name? <input type="text" name="person"><br>
+          <br>
+          Pick a "Compliment":
+          <select name="diss">
+            {diss}
+          </select><br>
+          <br>
+          <input type="submit" name="diss_button" value='Get "Compliment"'>
+
         </form>
       </body>
     </html>
-    """
+    """.format(compliment=comp_options, diss=diss_options)
 
 
 @app.route('/greet')
@@ -47,8 +85,7 @@ def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
-
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
@@ -61,6 +98,26 @@ def greet_person():
       </body>
     </html>
     """.format(player=player, compliment=compliment)
+
+
+@app.route('/diss')
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    diss = request.args.get("diss")
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {diss}!
+      </body>
+    </html>
+    """.format(player=player, diss=diss)
 
 
 if __name__ == '__main__':
